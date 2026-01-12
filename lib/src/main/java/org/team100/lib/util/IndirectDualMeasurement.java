8f5e9cef69fc8533b6196e7b8c54a2c361785daa
Joel Truher <joel@truher.org>
2026-01-12 08:29:01 -0800

    chinese remainder theorem challenge

---
+++ lib/src/main/java/org/team100/lib/util/IndirectDualMeasurement.java
@@ -0,0 +1,47 @@
+package org.team100.lib.util;
+
+/**
+ * Math to support indirect measurment of the position a large gear (e.g. a
+ * turret).
+ * 
+ * The general idea is to use two encoders, each geared to the large gear using
+ * different, coprime ratios.
+ * 
+ * So, given sensor gear tooth counds m1 and m2, we want to find x (the big gear
+ * position), where we only know the remainders, a1 and a2:
+ * 
+ * x mod m1 = a1
+ * x mod m2 = a2
+ * 
+ * This is similar to the "Chinese Remainder Theorem," but in this case the
+ * measurements are continuous, not integers.
+ */
+public class IndirectDualMeasurement {
+
+    /** final gear teeth */
+    private final int m_N;
+    /** sensor 1 gear teeth */
+    private final int m_m1;
+    /** sensor 2 gear teeth */
+    private final int m_m2;
+
+    /**
+     * @param N  final gear teeth
+     * @param m1 sensor 1 gear teeth
+     * @param m2 sensor 2 gear teeth
+     */
+    public IndirectDualMeasurement(int N, int m1, int m2) {
+        m_N = N;
+        m_m1 = m1;
+        m_m2 = m2;
+    }
+
+    /**
+     * @param a1 sensor 1 measurement in teeth
+     * @param a2 sensor 2 measurement in teeth
+     */
+    public double x(double a1, double a2) {
+        return 0;
+    }
+
+}
