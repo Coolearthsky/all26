ccb7aed24abdad88fc237bff968ba487c1dde604
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2026-01-23 16:24:24 -0800

    bugfix with lib fixed in study

--- studies/serialiserTest/src/main/java/frc/team100/frc2026/Robot.java
+++ studies/serialiserTest/src/main/java/frc/team100/frc2026/Robot.java
@@ -2,9 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.team100.frc2026;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
